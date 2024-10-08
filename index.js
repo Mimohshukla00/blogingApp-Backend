f8b1903527990419275a88b8ff9ab46fd76d5e57
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-10-08 22:25:23 +0530

    port activation

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 
 const app = express();
+require("dotenv").config();
 
-app.listen(POR)
+const PORT = process.env.PORT || PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
